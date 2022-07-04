cf83376f63332c009655f75d9ac71fc417e5db77
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-07-04 16:27:37 +0530

    first commit

---
+++ src/components/BudgetCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function BudgetCard( {name, amount, max}) {
+  return (
+    <Card>
+        <Card.Body>
+            <Card.Title>
+                <div>{name}</div>
+                <div>{amount} / {max}</div>
+            </Card.Title>
+        </Card.Body>
+
+    </Card>
+  )
+}
+
+export default BudgetCard
