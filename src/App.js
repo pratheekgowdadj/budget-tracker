cb3005134a49535c05ed6fa67af0fcd53dce76c7
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-09-23 19:25:11 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
-      <Container className='my-4'>
-          <Stack direction="horizontal" gap="2" className="nb-4">
-            <h1 className='me-auto'>Budgets</h1>
-            <Button variant='primary'>Add Budget</Button>
-            <Button variant='outline-primary'>Add Expense</Button>
-          </Stack>
-          <div style={{ display:"grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px,1fr))",
-            gap:"1rem", alignItems:"flex-start"}}></div>
-        <BudgetCard></BudgetCard>
-      </Container>
+      <><Container className='my-4'>
+      <Stack direction="horizontal" gap="2" className="nb-4">
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div style={{
+        display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px,1fr))",
+        gap: "1rem", alignItems: "flex-start"
+      }}></div>
+      <BudgetCard></BudgetCard>
+    </Container><Button className='margin-20px'>Click here</Button></>
   );
 }
 
